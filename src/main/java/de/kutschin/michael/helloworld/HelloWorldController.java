222bd52dfa8cd1752b60f48b52dbc24e96c5f006
Michael Kutschin <michael.kutschin@st.oth-regensburg.de>
2019-01-05 23:28:48 +0100

    Editted some unimportant stuff

--- src/main/java/de/kutschin/michael/helloworld/HelloWorldController.java
+++ src/main/java/de/kutschin/michael/helloworld/HelloWorldController.java
@@ -2,14 +2,15 @@ package de.kutschin.michael.helloworld;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/v1/hello")
 public class HelloWorldController {
 	
-	@RequestMapping("/world")
-    public String index() {
-        return "Hello World!";
+	@RequestMapping("/{name}")
+    public String index(@PathVariable String name) {
+        return "Hello " + name + "!";
     }
 	
 }
